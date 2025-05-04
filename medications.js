b148787bd7f1248d0f86f5d0d1dc207c7f2df534
medisuresafety <nikkinatividadss@gmail.com>
2025-05-04 13:02:52 +0800

    Create medications.js

---
+++ medications.js
@@ -0,0 +1,93 @@
+const medicationDatabase = {
+    "480778841769": {
+        name: "Carvedilol",
+        brand: "Carvid",
+        dosage: "3.125mg, 6.25mg, 12.5mg, 25mg",
+        indication: "Hypertension, Heart Failure",
+        route: "Oral"
+    },
+    "000780229980": {
+        name: "Metformin",
+        brand: "Glucophage",
+        dosage: "500mg, 850mg, 1000mg",
+        indication: "Type 2 Diabetes",
+        route: "Oral"
+    },
+    "00023900101": {
+        name: "Aspirin",
+        brand: "Ascriptin",
+        dosage: "81mg, 325mg",
+        indication: "Pain Relief, Inflammation, Fever",
+        route: "Oral"
+    },
+    "000051300401": {
+        name: "Paracetamol",
+        brand: "Tempra",
+        dosage: "500mg, 650mg",
+        indication: "Pain Relief, Fever",
+        route: "Oral"
+    },
+    "000030715212": {
+        name: "Loratadine",
+        brand: "Claritin",
+        dosage: "10mg",
+        indication: "Allergic Rhinitis, Hay Fever, Urticaria",
+        route: "Oral"
+    },
+    "000052144233": {
+        name: "Amlodipine",
+        brand: "Norvasc",
+        dosage: "2.5mg, 5mg, 10mg",
+        indication: "Hypertension, Angina",
+        route: "Oral"
+    },
+    "000036199925": {
+        name: "Ibuprofen",
+        brand: "Advil",
+        dosage: "200mg, 400mg, 600mg, 800mg",
+        indication: "Pain Relief, Inflammation, Fever",
+        route: "Oral"
+    },
+    "000110400527": {
+        name: "Cough Syrup",
+        brand: "Robitussin",
+        dosage: "10mg/5ml",
+        indication: "Cough, Cold",
+        route: "Oral"
+    },
+    "000023845232": {
+        name: "Furosemide",
+        brand: "Lasix",
+        dosage: "20mg, 40mg, 80mg",
+        indication: "Edema, Hypertension",
+        route: "Oral"
+    },
+    "000042845573": {
+        name: "Omeprazole",
+        brand: "Losec",
+        dosage: "10mg, 20mg, 40mg",
+        indication: "GERD, Stomach Ulcers",
+        route: "Oral"
+    },
+    "000086482589": {
+        name: "Glimepiride",
+        brand: "Amaryl",
+        dosage: "1mg, 2mg, 4mg",
+        indication: "Type 2 Diabetes",
+        route: "Oral"
+    },
+    "000019501082": {
+        name: "Atorvastatin",
+        brand: "Lipitor",
+        dosage: "10mg, 20mg, 40mg, 80mg",
+        indication: "Hyperlipidemia, Cardiovascular Disease",
+        route: "Oral"
+    },
+    "000078249023": {
+        name: "Salbutamol",
+        brand: "Ventolin",
+        dosage: "100mcg, 200mcg",
+        indication: "Asthma, Chronic Obstructive Pulmonary Disease (COPD)",
+        route: "Inhalation"
+    }
+};
